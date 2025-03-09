9b56ceb8360a85d0f670343f482a7337e0bd52de
Carlos <ch.akim1994@gmail.com>
2025-03-09 12:14:32 -0300

    Testando o pygame

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import pygame
+
+print("Setup Start")
+pygame.init()
+
+window = pygame.display.set_mode(size=(600,480))
+print("Setup end")
+
+print("Loop Start")
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+            print("Fim do loop")
