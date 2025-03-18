100bf380f473103ca85d86a70436de1616ed0ff2
Carlos Akim <Ch.akim1994@gmail.com>
2025-03-18 15:52:27 -0300

    Foram adicionados os arquivos a pasta Asset

--- code/Game.py
+++ code/Game.py
@@ -6,14 +6,10 @@ from code.Menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(848, 477))
 
     def run(self):
-
         while True:
             menu = Menu(self.window)
             menu.run()
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-           #         pygame.quit()
-          #          quit()
+
--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,13 @@
+import pygame.image
+
+
 class Menu:
     def __init__(self, window):
-        self.window = window
+        self.window = window # Nossa Janela
+        self.surf = pygame.image.load("Asset/Battleground1.png") # Nosso background
+        self.rect = self.surf.get_rect(left=0, top=0) # Desenhamos um retangulo
 
     def run(self):
-        passs
+        self.window.blit(source=self.surf, dest=self.rect) # Vai ficar atualizando a imagem dentro do retangulo
+        pygame.display.flip() #Atualiza nosso Display
+        pass
