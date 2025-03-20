023155fd9846056c923b60b2f7481722028d705f
Carlos Akim <Ch.akim1994@gmail.com>
2025-03-20 13:00:35 -0300

    Foram acrescentados musica e imagem no menu do game

--- code/Game.py
+++ code/Game.py
@@ -1,14 +1,16 @@
 import pygame
 
+from code.Const import WIN_HEIGHT, WIN_WIDTH
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(848, 477))
+        self.window = pygame.display.set_mode(size=(WIN_HEIGHT, WIN_WIDTH))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
--- code/Menu.py
+++ code/Menu.py
@@ -8,6 +8,16 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0) # Desenhamos um retangulo
 
     def run(self):
-        self.window.blit(source=self.surf, dest=self.rect) # Vai ficar atualizando a imagem dentro do retangulo
-        pygame.display.flip() #Atualiza nosso Display
-        pass
+        pygame.mixer_music.load('Asset/Menu.wav')  # adicionando musica ao Menu
+        pygame.mixer_music.play(-1)  # A musica vai ficar tocando eternamente
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)  # Vai ficar atualizando a imagem dentro do retangulo
+            pygame.display.flip()  # Atualiza nosso Display
+
+
+            #Check for all events
+            for events in pygame.event.get():
+                if events.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
